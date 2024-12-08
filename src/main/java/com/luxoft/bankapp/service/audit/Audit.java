982b7b73af5b8b83fe8f2adaf00f34c46369a691
onelessangel <fromtheotherside04@gmail.com>
2024-12-08 13:11:20 +0200

    5.1 + 5.2 - Intro + audit service

---
+++ src/main/java/com/luxoft/bankapp/service/audit/Audit.java
@@ -0,0 +1,9 @@
+package com.luxoft.bankapp.service.audit;
+
+public interface Audit {
+    void auditDeposit(long accountId, double amount);
+
+    void auditWithdraw(long accountId, double amount, WithdrawState state);
+
+    void auditBalance(long accountId);
+}
